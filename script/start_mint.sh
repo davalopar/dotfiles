7ca7b1e41291671940d8146482602e069f727e2d
Rafi <rafiblecher@gmail.com>
2014-08-10 17:21:39 +1000

    Mint first-time script. Not a dotfile, just need to run it.

---
+++ script/start_mint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get purge banshee libreoffice-* hexchat* pidgin* thunderbird* gimp* brasero*
+sudo apt-get install nemo-dropbox exuberant-ctags vim clementine xfce4-power-manager git redshift chromium-browser awesome python-virtualenv python-dev
