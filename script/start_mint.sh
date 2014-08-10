479fdc6f5d91e3c4fcbdf76e5eb7919b699158b4
Rafi <rafiblecher@gmail.com>
2014-08-10 22:21:21 +1000

    Added python-pip to start_mint.sh

--- script/start_mint.sh
+++ script/start_mint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get purge banshee libreoffice-* hexchat* pidgin* thunderbird* gimp* brasero*
-sudo apt-get install nemo-dropbox exuberant-ctags vim clementine xfce4-power-manager git redshift chromium-browser awesome python-virtualenv python-dev
+sudo apt-get install nemo-dropbox exuberant-ctags vim clementine xfce4-power-manager git redshift chromium-browser awesome python-pip python-virtualenv python-dev
