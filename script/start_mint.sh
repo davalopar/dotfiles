953c80e8147becc19212340ab7de1e321c5c1274
Rafi Blecher <rafiblecher@gmail.com>
2014-08-12 20:24:51 +1000

    Added git-completion to start_mint.sh and structured install command/s

--- script/start_mint.sh
+++ script/start_mint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo apt-get purge banshee libreoffice-* hexchat* pidgin* thunderbird* gimp* brasero*
-sudo apt-get install nemo-dropbox exuberant-ctags vim clementine xfce4-power-manager git gitg redshift chromium-browser awesome python-pip python-virtualenv python-dev tidy
+sudo apt-get install awesome xfce4-power-manager
+sudo apt-get install chromium-browser
+sudo apt-get install clementine
+sudo apt-get install git gitg bash-completion
+sudo apt-get install nemo-dropbox
+sudo apt-get install python-pip python-virtualenv python-dev
+sudo apt-get install redshift
+sudo apt-get install vim exuberant-ctags tidy
