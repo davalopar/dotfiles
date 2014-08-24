9927c340e6bd5d70b89d91319073f276acafeef1
Rafi Blecher <rafiblecher@gmail.com>
2014-08-24 21:28:25 +1000

    Added latex and pgf to start_mint.sh

--- script/start_mint.sh
+++ script/start_mint.sh
@@ -5,6 +5,7 @@ sudo apt-get install chromium-browser
 sudo apt-get install clementine
 sudo apt-get install git gitg bash-completion
 sudo apt-get install nemo-dropbox
+sudo apt-get install pgf texlive-latex-extra
 sudo apt-get install python-pip python-virtualenv python-dev
 sudo apt-get install redshift
 sudo apt-get install shutter
